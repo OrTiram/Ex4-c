9b7d0f0f22c2d820184797694b7ad951e314a21a
OrTiram <74321681+OrTiram@users.noreply.github.com>
2021-01-15 13:24:52 +0200

    Update TRIE.h
    define

--- TRIE.h
+++ TRIE.h
@@ -1,3 +1,5 @@
+#ifndef TRIE_H
+#define TRIE_H
 #include <stdlib.h>
 #define NUM_LETTERS ((int)26)
 #define Char_To_Index(c) ((int)c - (int)'a')
@@ -10,3 +12,5 @@ void Node_free(node* n);
 void TRIE_insert(node** head,char* str);
 void print_TRIE(node** head,char word[], int level, int regular);
 void freeTrie(node** head);
+
+#endif
