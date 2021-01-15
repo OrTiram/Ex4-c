a88f05019919957c0c3be0981a86e61a8e5af578
OrTiram <74321681+OrTiram@users.noreply.github.com>
2021-01-15 13:25:50 +0200

    Update TRIE.h
    delet void node free

--- TRIE.h
+++ TRIE.h
@@ -8,7 +8,6 @@
 struct node;
 typedef struct node node;
 node* Node_alloc(char letter, long unsigned int count, int isEndWord);
-void Node_free(node* n);
 void TRIE_insert(node** head,char* str);
 void print_TRIE(node** head,char word[], int level, int regular);
 void freeTrie(node** head);
